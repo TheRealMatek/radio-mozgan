2d469163c0218e44e6bf3406abafa8a3c4bbb54d
Luka Zontar <luka.zontar@celtra.com>
2023-12-30 13:34:50 +0100

    Move sample generation to separate thread

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ from Unicorn import Unicorn
 # Music generation code adapted from https://medium.com/@andrewadiletta/producing-music-with-rules-python-tutorial-8c4005f276f0
 
 soundFontPath = "./FluidR3_GM.sf2"
-simulation = False
+simulation = True
 
 if platform.system() == "Linux":
     fluidsynth.init(soundFontPath, "pulseaudio")
@@ -32,6 +32,7 @@ average = 0
 lastSampleTime = 0
 
 def generateSamples():
+    global average, deviation
     print("Generating samples")
     if simulation:
         eeg = nk.eeg_simulate(duration=1, sampling_rate=25)
@@ -43,41 +44,36 @@ def generateSamples():
     power_by_channels = power.mean(numeric_only=True, axis=0)
     deviation = np.std(power_by_channels)
     average = np.mean(power_by_channels)
-    return average, deviation
 
 def playMusic():
-    global average
-    global averageCutoff
-    while True:
-        for i in range(len(chords)):
-            current_chord = NoteContainer(chords[i])
-            base_note = Note(current_chord[0].name)
-            base_note.octave_down()
-            fluidsynth.play_Note(base_note)
-            time.sleep(1) 
-            # Play highest note in chord
-            fluidsynth.play_Note(current_chord[-1])  
-            # 50% chance on a bass note
-            if average > averageCutoff:
-                second_base_note = Note(current_chord[1].name)
-                second_base_note.octave_down()
-                fluidsynth.play_Note(second_base_note)
-            time.sleep(0.5)       
-            # 50% chance on a ninth
-            if average > averageCutoff:
-                ninth_note = Note(intervals.third(current_chord[0].name, key))
-                ninth_note.octave_up()
-                fluidsynth.play_Note(ninth_note)
-            time.sleep(0.5)     
-            # 50% chance on a last note
-            if average > averageCutoff:
-                fluidsynth.play_Note(current_chord[-2])
-            time.sleep(0.25)
+    for i in range(len(chords)):
+        current_chord = NoteContainer(chords[i])
+        base_note = Note(current_chord[0].name)
+        base_note.octave_down()
+        fluidsynth.play_Note(base_note)
+        time.sleep(1) 
+        # Play highest note in chord
+        fluidsynth.play_Note(current_chord[-1])  
+        # 50% chance on a bass note
+        if average > averageCutoff:
+            second_base_note = Note(current_chord[1].name)
+            second_base_note.octave_down()
+            fluidsynth.play_Note(second_base_note)
+        time.sleep(0.5)       
+        # 50% chance on a ninth
+        if average > averageCutoff:
+            ninth_note = Note(intervals.third(current_chord[0].name, key))
+            ninth_note.octave_up()
+            fluidsynth.play_Note(ninth_note)
+        time.sleep(0.5)     
+        # 50% chance on a last note
+        if average > averageCutoff:
+            fluidsynth.play_Note(current_chord[-2])
+        time.sleep(0.25)
 
-Thread(target=playMusic).start()
 while True:
+    playMusic()
     if lastSampleTime + sampleDuration < time.time():
-        average, deviation = generateSamples()
-        print(average)
+        Thread(target = generateSamples).start()
         lastSampleTime = time.time()
         # Thread(target = generateSamples).start()
